c8da1f16ec1657e79657dfe17fbe04f0e97dd094
Tyler Hughes <artman41@gmail.com>
2024-01-01 15:10:47 +0000

    Bump version

--- Statics.cs
+++ Statics.cs
@@ -4,7 +4,7 @@ namespace PlayerKillCounter {
     public static class Statics {
         public const string GUID = "PlayerKillCounter";
         public const string NAME = "Player Kill Counter";
-        public const string VERSION = "0.0.1";
+        public const string VERSION = "0.0.2";
 
         public const uint NETWORK_ID = 100000;
     }
